66a7b8a55052aabe305fd2f053b66fc107440867
ahmedelmansi-tech <ahmed.helmii911@gmail.com>
2025-11-11 21:24:39 +0200

    shaping the skelton

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthPage from "./pages/AuthPage";
+import Nav from "./components/Nav";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1>Supabase App </h1>
-    </>
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<AuthPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import { FaHome } from "react-icons/fa";
+
+const Nav = () => {
+  return (
+    <nav>
+      <FaHome />
+    </nav>
+  );
+};
+
+export default Nav;
